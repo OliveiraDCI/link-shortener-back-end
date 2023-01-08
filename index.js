31be83d95c6e32942dae2b6245f400575ef2eed5
OliveiraDCI <oliveira.sein.dci@gmail.com>
2023-01-08 21:09:26 +0100

    Correction in content security policy.

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(
   helmet.contentSecurityPolicy({
     directives: {
       defaultSrc: ["'self'"],
-      styleSrc: ["'self'", "https://fonts.googleapis.com"],
+      styleSrc: ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com"],
     },
   })
 );
